8e7d5e519a885242cd892d0b5d5469cb1e74f46a
Sherry Li <sherryli0127@gmail.com>
2021-04-27 17:28:27 -0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,30 @@
-import ApolloClient from 'apollo-boost'
+import ApolloClient from "apollo-boost";
 // import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-import {ApolloProvider} from 'react-apollo'
+import {ApolloProvider} from "react-apollo";
 // import { ApolloProvider } from '@apollo/client';
 
-
 //components
-import BookList from './components/BookList'
-import AddBook from './components/AddBook'
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
 
 //apollo client setup
 const client = new ApolloClient({
   //the endpoint making queries to
-  uri:'http://localhost:4000/graphql',
+  // uri: "http://localhost:4000/graphql",
+  uri: "https://graphql-readinglist.herokuapp.com/graphql",
   // cache: new InMemoryCache()
-
-})
+});
 
 function App() {
   return (
     <ApolloProvider client={client}>
-
       <div id="main">
-        <h1>Ninja's Reading List</h1>
+        <h1>Sherry's Reading List</h1>
         <BookList></BookList>
         <AddBook></AddBook>
       </div>
     </ApolloProvider>
-
   );
 }
 
--- server/app.js
+++ server/app.js
@@ -1,28 +1,37 @@
-const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/schema')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require("express");
+const {graphqlHTTP} = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 //allow cross-origin requests
 app.use(cors());
 
-//connect to mlab database(mongodb atlas for new version)
-mongoose.connect('mongodb+srv://lxr:test123@gql-ninja.a9u8c.mongodb.net/<dbname>?retryWrites=true&w=majority')
-//once is a event listener
-mongoose.connection.once('open', ()=>{
-    console.log("connected to database");
-})
-
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql:true
-}));
-
-app.listen(4000, () => {
-    console.log('now listening for requests on port 4000')
+app.get("/", (req, res) => {
+  res.status(200).send("hello reading list");
 });
 
+//connect to mlab database(mongodb atlas for new version)
+mongoose.connect(
+  "mongodb+srv://lxr:test123@cluster0.a9u8c.mongodb.net/gql?retryWrites=true&w=majority"
+);
+//once is a event listener
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
+const PORT = process.env.PORT || 4000;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log("now listening for requests on port 4000");
+});
